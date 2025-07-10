926413be7a8d5b846d8b93c69aa68512e169299c
Princekush29 <princekumarkushwaha@gmail.com>
2025-07-10 19:29:23 +0530

    Leetcode programs

--- src/leetCode/AddStrings415.java
+++ src/leetCode/AddStrings415.java
@@ -4,12 +4,30 @@ public class AddStrings415 {
 
 	public static void main(String[] args) {
 		String num1 = "4", num2 = "11";
-		System.out.println("Output is " + addStrings(num1, num2));
 		System.out.println("Output is " + addStrings1(num1, num2));
+		System.out.println("Output is " + addStrings2(num1, num2));
+		System.out.println("Output is " + addStrings3(num1, num2));
 	}
 
+	//Method 3 - without using any inbuilt functions
+	public static String addStrings1(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int len1 = num1.length() - 1;
+		int len2 = num2.length() - 1;
+		
+		int carry = 0;
+		while (len1 >= 0 || len2 >= 0 || carry != 0) {
+			if (len1 >= 0)
+				carry = carry + num1.charAt(len1--) - '0';
+			if (len2 >= 0)
+				carry = carry + num2.charAt(len2--) - '0';
+			sb.append(carry % 10);
+			carry = carry / 10;
+		}
+		return sb.reverse().toString();
+	}
 	// Method 1 - without using any inbuilt functions
-	public static String addStrings(String num1, String num2) {
+	public static String addStrings2(String num1, String num2) {
 		int borrow = 0, c, reminder;
 		if (num1.length() > num2.length())
 			num2 = modifyString(num1, num2);
@@ -54,7 +72,7 @@ public class AddStrings415 {
 	}
 
 	// Method 2 - using inbuilt functions
-	public static String addStrings1(String a, String b) {
+	public static String addStrings3(String a, String b) {
 		int num1 = Integer.parseInt(a);
 		int num2 = Integer.parseInt(b);
 		System.out.println("Number 1 " + num1);
--- src/leetCode/Test1.java
+++ src/leetCode/Test1.java
@@ -3,42 +3,25 @@ package leetCode;
 public class Test1 {
 
 	public static void main(String[] args) {
-		String a = "1010", b = "1010";
-		System.out.println("Output is " + addBinary1(a, b));
+		String num1 = "4", num2 = "11";
+		System.out.println("Output is " + addStrings1(num1, num2));
 	}
 
-	public static String addBinary1(String a, String b) {
+	// Method 3 - without using any inbuilt functions
+	public static String addStrings1(String num1, String num2) {
+		StringBuilder sb = new StringBuilder();
+		int len1 = num1.length()-1;
+		int len2 = num2.length()-1;
 		int carry = 0;
-		String newStr = "";
-		for (int i = a.length() - 1; i >= 0; i--) {
-			if (a.charAt(i) == '0' && b.charAt(i) == '0') {
-				if (carry == 0) {
-					newStr = "0" + newStr;
-					carry = 0;
-				} else {
-					newStr = "1" + newStr;
-					carry = 0;
-				}
-			} else if ((a.charAt(i) == '0' && b.charAt(i) == '1') || (a.charAt(i) == '1' && b.charAt(i) == '0')) {
-				if (carry == 0) {
-					newStr = "1" + newStr;
-					carry = 0;
-				} else {
-					newStr = "0" + newStr;
-					carry = 1;
-				}
-			} else if (a.charAt(i) == '1' && b.charAt(i) == '1') {
-				if (carry == 0) {
-					newStr = "0" + newStr;
-					carry = 1;
-				} else {
-					newStr = "1" + newStr;
-					carry = 1;
-				}
+		while (len1 >= 0 || len2 >= 0 || carry != 0) {
+			if (len1 >= 0) 
+				carry = carry + num1.charAt(len1--) - '0';
+			if (len2 >= 0) {
+				carry = carry + num2.charAt(len2--) - '0';
 			}
+			sb.append(carry % 10);
+			carry = carry / 10;
 		}
-		if (carry == 1)
-			newStr = "1" + newStr;
-		return newStr;
+		return sb.reverse().toString();
 	}
 }
