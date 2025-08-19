c3ed8a818a325665fb7193526ecc5e0b80bbbd78
Princekush29 <princekumarkushwaha@gmail.com>
2025-08-19 16:58:22 +0530

    Update Constructor.java

---
+++ src/programs/Constructor.java
@@ -0,0 +1,12 @@
+package programs;
+
+public class Constructor {
+
+	public void Constructor() {
+		System.out.println("I am Constructor");
+	}
+
+	public static void main(String[] args) {
+		Constructor obj = new Constructor();
+	}
+}
