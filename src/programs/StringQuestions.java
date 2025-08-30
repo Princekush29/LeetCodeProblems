90998c559cd51d668b68e2c4f93ed25d043d8420
Princekush29 <princekumarkushwaha@gmail.com>
2025-08-30 18:19:55 +0530

    updated

--- src/programs/StringQuestions.java
+++ src/programs/StringQuestions.java
@@ -2,6 +2,9 @@ package programs;
 
 public class StringQuestions {
 	public static void main(String args[]) {
+		System.out.println(".equals will check the value");
+		System.out.println("== will check the address");
+
 		// Q1
 		String str = "ONE" + 1 + 2 + "TWO" + "THREE" + 3 + 4 + "FOUR" + "FIVE" + 5;
 		System.out.println(str);
@@ -10,25 +13,22 @@ public class StringQuestions {
 		System.out.println(str10);
 
 		// Q2
-		String str1 = "Java";
-		String str2 = new String("Java");
-		String str3 = str2.intern();
+		System.out.println("----Q2----");
+		String str1 = "Java"; // point to String constant pool
+		String str2 = new String("Java"); // point to Heap memory
+		String str3 = str2.intern(); // point to String constant pool
 
-		System.out.println(str1 == str2);
-		System.out.println(str1 == str3);
-		System.out.println(str2 == str3);
+		System.out.println(str1 == str2);// false
+		System.out.println(str1 == str3);// true
+		System.out.println(str2 == str3);// false
 		System.out.println(str1.equals(str2));
 		System.out.println(str1.equals(str3));
 
 		// Q3
-		String str5 = null;
-		System.out.println(str5 + "hello");
-		System.out.println(str5.length());
-		
-		//Q4
-		String s1 = "abc";
-		String s2 = s1;
-		String s3 = new String("abc");
+		System.out.println("----Q3----");
+		String s1 = "abc"; // point to String constant pool
+		String s2 = s1; // point to String constant pool reference
+		String s3 = new String("abc"); // point to heap memory
 		String s4 = s1 + "def";
 
 		System.out.println(s1.equals(s2));// - True
@@ -39,6 +39,14 @@ public class StringQuestions {
 		System.out.println(s1 == s4);// - False
 		System.out.println(s1 == s3);// - False
 
+		// Yes, the intern() method is used to redirect a heap-based string reference to
+		// the String Constant Pool (SCP) — but only if an identical string already
+		// exists there, or it adds it if not.
+		System.out.println("After s3 point to string constant pool");
+		// if i want s3 to point to String constant pool
+		s3 = new String("abc").intern();
+		System.out.println(s2 == s3);
+
 		System.out.println("---");
 		String a = "Apple";
 		String b = "Apple";
@@ -48,5 +56,12 @@ public class StringQuestions {
 
 		System.out.println(a == c);
 		System.out.println(a.equals(c));
+
+		// Q4
+		System.out.println("----Q4----");
+		String str5 = null;
+		System.out.println(str5 + "hello");
+		System.out.println(str5.length());
+
 	}
 }
--- src/programs/Test.java
+++ src/programs/Test.java
@@ -1,20 +1,32 @@
 package programs;
 
-import java.util.Arrays;
-
 public class Test {
 
 	public static void main(String[] args) {
 
-		int a1[] = { 1, 2, 5, 4, 6 }; // [6, 4, 5, 3, 2, 1]
+		int arr[] = { 1, 4, 20, 3, 10, 5 };
+		int sum = 33, newSum = 0, token = 1;
+		for (int i = 0, j = i + 1; j < arr.length; j++) {
+			int a = arr[i];
+			if (token == 1) {
+				newSum = newSum + a;
+				newSum = newSum + arr[j];
+				token = 2;
 
-		for (int i = 0, j = a1.length - 1; i < a1.length / 2; i++, j--) {
-			int temp = a1[j];
-			a1[j] = a1[i];
-			a1[i] = temp;
-
+			} else {
+				newSum = newSum + arr[j];
+			}
+			if (sum == newSum) {
+				System.out.println(i);
+				System.out.println(j);
+				break;
+			}
+			if (j == arr.length - 1 || newSum > sum) {
+				token = 1;
+				i = i + 1;
+				j = i;
+				newSum = 0;
+			}
 		}
-		System.out.println(Arrays.toString(a1));
-
 	}
 }
