65250a7a87d4bdc3df14d4f6ee9e7851dd29ca35
Prince Kushwaha <princekumarkushwaha@gmail.com>
2026-01-03 22:34:36 +0530

    updated

--- src/programs/Constructor.java
+++ src/programs/Constructor.java
@@ -6,7 +6,13 @@ public class Constructor {
 		System.out.println("I am Constructor");
 	}
 
+	public Constructor(int a) {
+		//this();
+		System.out.println("I am parametrized Constructor");
+	}
+
 	public static void main(String[] args) {
-		Constructor obj = new Constructor();
+		Constructor obj1 = new Constructor();
+		Constructor obj = new Constructor(10);
 	}
 }
--- src/programs/Test1111.java
+++ src/programs/Test1111.java
@@ -1,56 +1,34 @@
 package programs;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.Set;
 
 public class Test1111 {
 
 	public static void main(String[] args) {
 
-		Integer[] a1 = { 1, 2, 3, 6, 5, 2, 1 };
-		Integer[] a2 = { 1, 2, 6, 5, 3 };
-		Integer[] a3 = { 1, 2, 3, 4, 6 };
-		commonElementUsingMap(a1, a2, a3);
-		commonElementUsingHashMap(a1, a2, a3);
+		String str = "geekksforgggeeeeks";
+		// O/p = [ee, kk, ggg, eeee]
+		System.out.println(consecutiveChar(str));
 	}
 
-	public static void commonElementUsingMap(Integer a1[], Integer a2[], Integer a3[]) {
-
-		HashSet<Integer> set1 = new HashSet<>(Arrays.asList(a1));
-		HashSet<Integer> set2 = new HashSet<>(Arrays.asList(a2));
-		HashSet<Integer> set3 = new HashSet<>(Arrays.asList(a3));
-
-		set1.retainAll(set2);
-		set1.retainAll(set3);
-		System.out.println(set1);
-	}
-
-	public static void commonElementUsingHashMap(Integer a1[], Integer a2[], Integer a3[]) {
-
-		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-		for (int i = 0; i < a1.length; i++) {
-			if (!(hm.containsKey(a1[i]))) {
-				hm.put(a1[i], 1);
+	public static Set<String> consecutiveChar(String str) {
+		String newStr = "";
+		HashSet<String> al = new HashSet<String>();
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.charAt(i) == str.charAt(i + 1)) {
+				if (newStr.length() < 2) {
+					newStr = newStr + str.charAt(i) + str.charAt(i + 1);
+				} else {
+					newStr += str.charAt(i);
+				}
+			} else {
+				if (newStr.length() > 1) {
+					al.add(newStr);
+					newStr = "";
+				}
 			}
 		}
-		System.out.println(hm);
-		for (int i = 0; i < a2.length; i++) {
-			if (hm.containsKey(a2[i])) {
-				hm.put(a2[i], 2);
-			}
-		}
-		System.out.println(hm);
-		for (int i = 0; i < a3.length; i++) {
-			if (hm.containsKey(a3[i])) {
-				hm.put(a3[i], 3);
-			}
-		}System.out.println(hm);
-		for (Map.Entry<Integer, Integer> hm1 : hm.entrySet()) {
-			if (hm1.getValue() == 3) {
-				System.out.println(hm1.getKey());
-			}
-		}
+		return al;
 	}
 }
